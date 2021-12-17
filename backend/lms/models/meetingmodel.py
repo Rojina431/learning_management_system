34831153882c173b1b6c33da31c0e599d75f257f
Rojina431 <rojinabaral28@gmail.com>
2021-12-16 20:54:59 -0500

    meeting schedule

--- backend/lms/models/meetingmodel.py
+++ backend/lms/models/meetingmodel.py
@@ -7,6 +7,6 @@ class MeetingModel(models.Model):
     meeting_title = models.CharField(max_length=200)
     meeting_url = models.URLField(max_length=200)
     teacher_created = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    meeting_created_data = models.DateTimeField(auto_now=True)
+    meeting_created_date = models.DateTimeField(auto_now=True)
     meeting_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
