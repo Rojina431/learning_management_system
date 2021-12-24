f9137132f6b4e6225685ef491edcfc4c060ce667
Rojina431 <rojinabaral28@gmail.com>
2021-12-24 12:56:16 -0500

    recording

--- backend/lms/serializers/assignment_serializer.py
+++ backend/lms/serializers/assignment_serializer.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from ..models import assignmentmodel
-import pytz
 
 class AssignmentCreateSerializer(serializers.ModelSerializer):
    
--- backend/lms/serializers/recorded_serializer.py
+++ backend/lms/serializers/recorded_serializer.py
@@ -1,7 +1,8 @@
-from django.db.models import fields
+
 from rest_framework import serializers
 from ..models import recordedmodel
+
 class RecordedSerializer(serializers.ModelSerializer):
     class Meta:
-        model = recordedmodel
+        model = recordedmodel.RecordedModel
         fields = ['recording', 'subject', 'teacher']
